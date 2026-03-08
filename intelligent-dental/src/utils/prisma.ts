f931e41bebb45d7ace087ca555441d06425ae401
Chiraphat <nightfish1596@gmail.com>
2026-03-08 21:27:46 +0700

    prisma.config

--- intelligent-dental/src/utils/prisma.ts
+++ intelligent-dental/src/utils/prisma.ts
@@ -1,5 +1,5 @@
 // src/utils/prisma.ts
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@/generated/prisma/client"
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
